144a0bade265e1c5e5452a1484a1f86e37cf5bd6
pcphantom <117548056+pcphantom@users.noreply.github.com>
2025-11-06 19:00:01 -0800

    Breaking stuff
    Just having Claude make my life hell.

--- CardGameGD/CardGameGD/scripts/spells/army_upgrade.gd
+++
@@ -1,8 +0,0 @@
-extends BaseSpell
-class_name ArmyUpgrade
-
-func _init(p_game_state, p_card: Card, p_card_image, p_owner, p_opponent):
-	super(p_game_state, p_card, p_card_image, p_owner, p_opponent)
-
-func on_cast() -> void:
-	super.on_cast()
--- CardGameGD/CardGameGD/scripts/spells/ritual_of_the_forest.gd
+++
@@ -1,8 +0,0 @@
-extends BaseSpell
-class_name RitualOfTheForest
-
-func _init(game_ref, card_ref: Card, card_image_ref, owner_ref, opponent_ref) -> void:
-	super._init(game_ref, card_ref, card_image_ref, owner_ref, opponent_ref)
-
-func on_cast() -> void:
-	super.on_cast()
